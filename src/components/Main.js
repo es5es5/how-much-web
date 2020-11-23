69dc122e50c67ade3211d77e98537f5f99942def
es5es5 <es5e5s@nate.com>
2020-11-23 23:57:27 +0900

    react-odometerjs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+
+import Odometer from 'react-odometerjs';
+
 import './Main.scss'
 
 export default class Main extends Component {
   render() {
     return (
       <div id="main">
-        Main
+        <Odometer
+          value={99999}
+          format="dd"
+          auto={true}
+        />
       </div>
     )
   }
