6afa4cd8a1b10ff28b609487eacf969ed6488142
es5es5 <es5e5s@nate.com>
2020-11-24 00:04:21 +0900

    odometerjs init

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,23 @@ import Odometer from 'react-odometerjs';
 import './Main.scss'
 
 export default class Main extends Component {
+  state = {
+    odometerValue: 0
+  }
+
+  componentDidMount() {
+    this.setState({
+      odometerValue: 214122
+    })
+  }
+
   render() {
     return (
       <div id="main">
         <Odometer
-          value={99999}
-          format="dd"
+          value={this.state.odometerValue}
+          duration={600}
+          format="ddd,ddd,ddd"
           auto={true}
         />
       </div>
