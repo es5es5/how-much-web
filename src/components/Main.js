2cb51c72d5e241ebdf82b182066d01f40365b49e
es5es5 <es5e5s@nate.com>
2020-11-24 23:50:16 +0900

    Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,22 +11,36 @@ export default class Main extends Component {
 
   componentDidMount() {
     this.setState({
-      odometerValue: 214122
+      odometerValue: 142000
     })
   }
 
   render() {
     return (
       <div id="main">
-        <div className="count_wrap">
-          <span className="won">￦</span>
-          <Odometer
-            value={this.state.odometerValue}
-            duration={1000}
-            format="ddd,ddd,ddd"
-            auto={true}
-          />
-        </div>
+        <section className="info_wrap">
+          <p className="chatter">내가 이만큼이나 먹었다고?</p>
+          <div className="odometer_wrap">
+            <span className="won">￦</span>
+            <Odometer
+              value={this.state.odometerValue}
+              duration={300}
+              format="ddd,ddd,ddd"
+              auto={true}
+            />
+          </div>
+          <p className="myRank">현재 랭킹 <span className="number">1위</span></p>
+        </section>
+
+        <section className="rank_wrap">
+          <ul className="rank">
+            <li className="rank-item">
+              <span className="number">1</span>
+              <span className="userName">김루이</span>
+              <span className="pay">24,000</span>
+            </li>
+          </ul>
+        </section>
       </div>
     )
   }
