49a2c0d62b90852bbdd1ec048d7f43753e7209bd
es5es5 <es5e5s@nate.com>
2020-12-20 18:28:32 +0900

    formatting

--- src/components/PayCreate.js
+++ src/components/PayCreate.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import './PayCreate.scss'
+import "./PayCreate.scss";
 
 export class PayCreate extends Component {
   state = {
-    pay: '0',
-    payer: '',
-    payType: 'N빵'
-  }
+    pay: "0",
+    payer: "",
+    payType: "N빵",
+  };
   componentDidMount() {
     // set Payer to ME.
-    this.setState({ payer: '김루이' })
+    this.setState({ payer: "김루이" });
   }
-  onChange = event => {
-    const { name, value } = event.target
+  onChange = (event) => {
+    const { name, value } = event.target;
 
     switch (name) {
-      case 'pay': this.setState({ pay: value })
-        break
-      case 'payer': this.setState({ payer: value })
-        break
-      case 'payType': this.setState({ payType: value })
-        break
+      case "pay":
+        this.setState({ pay: value });
+        break;
+      case "payer":
+        this.setState({ payer: value });
+        break;
+      case "payType":
+        this.setState({ payType: value });
+        break;
       default:
-        break
+        break;
     }
-  }
+  };
   render() {
     return (
       <div id="payCreate">
@@ -34,12 +37,23 @@ export class PayCreate extends Component {
             <fieldset>
               <div className="row">
                 <label htmlFor="pay">얼마에요?</label>
-                <input id="pay" type="number" value={this.state.pay} name="pay" onChange={this.onChange} />
+                <input
+                  id="pay"
+                  type="number"
+                  value={this.state.pay}
+                  name="pay"
+                  onChange={this.onChange}
+                />
               </div>
 
               <div className="row">
                 <label htmlFor="payer">누가냄?</label>
-                <select name="payer" id="payer" value={this.state.payer} onChange={this.onChange}>
+                <select
+                  name="payer"
+                  id="payer"
+                  value={this.state.payer}
+                  onChange={this.onChange}
+                >
                   <option value="김루이">김루이</option>
                   <option value="일승이">일승이</option>
                 </select>
@@ -48,19 +62,35 @@ export class PayCreate extends Component {
               <div className="row">
                 <label htmlFor="payType">어케냄?</label>
                 <div className="check_wrap" onChange={this.onChange}>
-                  <input type="radio" id="payTypeN빵" name="payType" value="N빵" defaultChecked={this.state.payType === 'N빵'} />
-                  <label htmlFor="payTypeN빵" className="checkLabel">N빵</label>
+                  <input
+                    type="radio"
+                    id="payTypeN빵"
+                    name="payType"
+                    value="N빵"
+                    defaultChecked={this.state.payType === "N빵"}
+                  />
+                  <label htmlFor="payTypeN빵" className="checkLabel">
+                    N빵
+                  </label>
 
-                  <input type="radio" id="payType제각각" name="payType" value="제각각" defaultChecked={this.state.payType === '제각각'} />
-                  <label htmlFor="payType제각각" className="checkLabel">제각각</label>
+                  <input
+                    type="radio"
+                    id="payType제각각"
+                    name="payType"
+                    value="제각각"
+                    defaultChecked={this.state.payType === "제각각"}
+                  />
+                  <label htmlFor="payType제각각" className="checkLabel">
+                    제각각
+                  </label>
                 </div>
               </div>
             </fieldset>
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default PayCreate
+export default PayCreate;
