a26c9f1ed4b1dc5aee586c19ac4c55773e2eed56
es5es5 <es5e5s@nate.com>
2020-12-20 17:48:30 +0900

    Pay Form

--- src/components/PayCreate.js
+++ src/components/PayCreate.js
@@ -4,7 +4,9 @@ import './PayCreate.scss'
 
 export class PayCreate extends Component {
   state = {
-    pay: ''
+    pay: '',
+    payer: '',
+    payType: 'N빵'
   }
   componentDidMount() {
   }
@@ -13,9 +15,7 @@ export class PayCreate extends Component {
 
     switch (name) {
       case 'pay':
-        this.setState({
-          pay: value
-        })
+        this.setState({ pay: value })
         break
       default:
         break
@@ -24,12 +24,32 @@ export class PayCreate extends Component {
   render() {
     return (
       <div id="payCreate">
-        <div className="title_wrap">
-          <h2>얼마에요?</h2>
-        </div>
         <div className="form_wrap">
           <form onSubmit={() => {}}>
-            <input type="text" value={this.state.pay} name="pay" onChange={this.onChange} className="payInput" />
+            <fieldset>
+              <div className="row">
+                <label htmlFor="pay">얼마에요?</label>
+                <input id="pay" type="number" value={this.state.pay} name="pay" onChange={this.onChange} className="payInput" />
+              </div>
+
+              <div className="row">
+                <label htmlFor="payer">누가냄?</label>
+                <select name="payer" id="payer">
+                  <option value="">김루이</option>
+                </select>
+              </div>
+
+              <div className="row">
+                <label htmlFor="payType">어케냄?</label>
+                <div className="check_wrap">
+                  <input type="radio" id="payTypeN빵" name="payType" value="N빵" />
+                  <label htmlFor="payTypeN빵" className="checkLabel">N빵</label>
+
+                  <input type="radio" id="payType제각각" name="payType" value="제각각" />
+                  <label htmlFor="payType제각각" className="checkLabel">제각각</label>
+                </div>
+              </div>
+            </fieldset>
           </form>
         </div>
       </div>
